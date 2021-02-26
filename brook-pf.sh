7fbce1409e5fbb22234baef6e3ef47613271fafe
iiiiiii1 <25471406+iiiiiii1@users.noreply.github.com>
2021-02-26 21:39:43 +0800

    Update brook-pf.sh

--- brook-pf.sh
+++ brook-pf.sh
@@ -68,16 +68,7 @@ check_pid(){
 	PID=$(ps -ef| grep "brook relays"| grep -v grep| grep -v ".sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}')
 }
 check_new_ver(){
-	echo -e "请输入要下载安装的 Brook 版本号 ${Green_font_prefix}[ 格式是日期，例如: v20180909 ]${Font_color_suffix}
-版本列表请去这里获取：${Green_font_prefix}[ https://github.com/txthinking/brook/releases ]${Font_color_suffix}"
-	read -e -p "直接回车即自动获取:" brook_new_ver
-	if [[ -z ${brook_new_ver} ]]; then
-		brook_new_ver=$(wget -qO- https://api.github.com/repos/txthinking/brook/releases| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g')
-		[[ -z ${brook_new_ver} ]] && echo -e "${Error} Brook 最新版本获取失败！" && exit 1
-		echo -e "${Info} 检测到 Brook 最新版本为 [ ${brook_new_ver} ]"
-	else
-		echo -e "${Info} 开始下载 Brook [ ${brook_new_ver} ] 版本！"
-	fi
+	brook_new_ver=$(wget -qO- https://api.github.com/repos/txthinking/brook/releases| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g')
 }
 check_ver_comparison(){
 	brook_now_ver=$(${brook_file} -v|awk '{print $3}')
